e36624d595b859e217985fc67de43286c935879e
elw773 <45023245+elw773@users.noreply.github.com>
2020-05-06 20:57:48 -0400

    Completed InputManager class skeleton methods

--- src/InputManager.java
+++ src/InputManager.java
@@ -1,2 +1,48 @@
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.KeyEvent;
+
 public class InputManager {
+    private double mouseX;
+    private double mouseY;
+    private boolean mousePressed;
+    private boolean mouseClicked;
+    private boolean[] keyMap;
+    private boolean[] keyClickMap;
+    private Canvas canvas;
+
+    public InputManager(Canvas canvas){
+
+    }
+
+    public double getMouseX() {
+        return mouseX;
+    }
+
+    public double getMouseY() {
+        return mouseY;
+    }
+
+    public boolean isMousePressed(){
+        return mousePressed;
+    }
+
+    public boolean isMouseClicked() {
+        return mouseClicked;
+    }
+
+    public boolean[] getKeyMap() {
+        return keyMap;
+    }
+
+    public boolean[] getKeyClickMap() {
+        return keyClickMap;
+    }
+
+    public boolean isKeyPressed(KeyEvent key) {
+        return false;
+    }
+
+    public boolean isKeyClocked(KeyEvent key) {
+        return false;
+    }
 }
