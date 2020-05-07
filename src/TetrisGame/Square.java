a6f059e1b466e30a701dc2855b2e24cb92e10268
elw773 <45023245+elw773@users.noreply.github.com>
2020-05-06 20:46:12 -0400

    Completed Square enum and skeleton methods

--- src/TetrisGame/Square.java
+++ src/TetrisGame/Square.java
@@ -1,4 +1,11 @@
 package TetrisGame;
 
+import javafx.scene.canvas.GraphicsContext;
+
 public enum Square {
+    I, J, L, O, S, T, Z;
+
+    public static void draw(double gx, double gy, Square type, GraphicsContext graphicsContext){
+
+    }
 }
