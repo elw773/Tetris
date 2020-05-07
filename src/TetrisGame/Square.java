7a714ac86e1cc8c426908f2fa9dcae4bb516667e
elw773 <45023245+elw773@users.noreply.github.com>
2020-05-06 20:14:28 -0400

    Finished Tetromino.Base skeleton class

---
+++ src/TetrisGame/Square.java
@@ -0,0 +1,4 @@
+package TetrisGame;
+
+public enum Square {
+}
--- src/TetrisGame/Tetromino/Base.java
+++ src/TetrisGame/Tetromino/Base.java
@@ -1,5 +1,46 @@
 package TetrisGame.Tetromino;
 
-public class Base {
+import TetrisGame.Move;
+import TetrisGame.Square;
+import javafx.scene.canvas.GraphicsContext;
 
+public abstract class Base {
+    private double x;
+    private double y;
+
+    public boolean translate(Move.Direction direction, Square[][] board){
+        return false;
+    }
+
+    public boolean canTranslate(Move.Direction direction, Square[][] board){
+        return false;
+    }
+
+    public boolean fall(Square[][] board){
+        return false;
+    }
+
+    public boolean canFall(Square[][] board){
+        return false;
+    }
+
+    public void lock(Square[][] board){
+
+    }
+
+    public abstract boolean rotate(Move.Direction direction, Square[][] board);
+
+    public abstract boolean canRotate(Move.Direction direction, Square[][] board);
+
+    public void drawGhostRelative(double boardGx, double boardGy, double squareSize, GraphicsContext gc){
+
+    }
+
+    public void drawAbsolute(double gx, double gy, double squareSize, GraphicsContext gc){
+
+    }
+
+    public void drawRelative(double boardGx, double boardGy, double squareSize, GraphicsContext gc){
+
+    }
 }
