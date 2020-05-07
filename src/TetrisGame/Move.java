a4d61a09ec39f046a464c7475e5d213f3c4ae380
elw773 <45023245+elw773@users.noreply.github.com>
2020-05-06 20:50:52 -0400

    Completed Move enum skeleton methods

--- src/TetrisGame/Move.java
+++ src/TetrisGame/Move.java
@@ -2,7 +2,11 @@ package TetrisGame;
 
 public class Move {
     public enum Direction{
+        LEFT, RIGHT, NONE;
 
+        public int getSign(Direction direction){
+            return 0;
+        }
     }
 
 }
