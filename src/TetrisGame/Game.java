a84de9d98e8dfb57210924b618f06e3dc8ade920
elw773 <45023245+elw773@users.noreply.github.com>
2020-05-06 20:44:01 -0400

    Completed Game skeleton methods

--- src/TetrisGame/Game.java
+++ src/TetrisGame/Game.java
@@ -1,4 +1,54 @@
 package TetrisGame;
 
+import TetrisGame.Tetromino.Tetromino;
+
+import java.util.Queue;
+
 public class Game {
+    private int lockCounter;
+    private int dropCounter;
+    private Square[][] board;
+    private int score;
+    private Tetromino hold;
+    private Tetromino currentTetromino;
+    private Tetromino[] next;
+
+
+    public void update(Move move){
+
+    }
+
+    public boolean gameIsOver(){
+        return false;
+    }
+
+    public void reset() {
+    }
+
+    public boolean lineIsFull(int line){
+        return false;
+    }
+
+    public void clearLine(int line){
+    }
+
+    public Tetromino getHold(){
+        return null;
+    }
+
+    public Square[][] getBoard(){
+        return null;
+    }
+
+    public Tetromino getCurrentTetromino(){
+        return null;
+    }
+
+    public Tetromino[] getNext(){
+        return null;
+    }
+
+    public int getScore(){
+        return 0;
+    }
 }
