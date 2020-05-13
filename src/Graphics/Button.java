cd4c5e4983ba955aade9c5dbc4f37e3c539e62dc
elw773 <45023245+elw773@users.noreply.github.com>
2020-05-12 22:49:17 -0400

    Completed Button class skeleton methods

--- src/Graphics/Button.java
+++ src/Graphics/Button.java
@@ -1,4 +1,25 @@
 package Graphics;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Paint;
+
 public class Button {
+    private double x;
+    private double y;
+    private double height;
+    private double width;
+    private boolean shown;
+    private Runnable runnable;
+
+    public Button(double x, double y, double width, double height, String text, Paint color, Runnable runnable){
+
+    }
+
+    public void show(GraphicsContext gc){
+
+    }
+
+    public void hide(){
+
+    }
 }
