f1de8742f0e2a4021eee99fdff86b8daa99ead20
elw773 <45023245+elw773@users.noreply.github.com>
2020-05-15 01:05:47 -0400

    Created skeleton tetrisMenu methods

--- src/Graphics/TetrisMenu.java
+++ src/Graphics/TetrisMenu.java
@@ -1,13 +1,19 @@
 package Graphics;
 
 import Main.MoveGetter;
+import TetrisGame.Game;
+import javafx.scene.canvas.GraphicsContext;
 
 public class TetrisMenu {
+    private Game game;
+    private boolean paused;
+
+
     public void newGame(MoveGetter moveGetter){
 
     }
 
-    public void doFrame(){
-
+    public void doFrame(GraphicsContext gc){
+        
     }
 }
