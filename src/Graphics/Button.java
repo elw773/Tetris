66a7c9267573dfd4b8954aa30b5230ee8fe0c376
elw773 <45023245+elw773@users.noreply.github.com>
2020-05-23 17:48:18 -0400

    Finished button show method

--- src/Graphics/Button.java
+++ src/Graphics/Button.java
@@ -3,12 +3,17 @@ package Graphics;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
 
 public class Button {
     private double x;
     private double y;
     private double height;
     private double width;
+    private double outlineSize;
+    private double fontSize;
     private boolean shown;
     private String text;
     private Runnable runnable;
@@ -22,14 +27,10 @@ public class Button {
     }
 
     public static Color toTextColor(Color color){
-        if(color.getBrightness() > 0.5){
-            return Color.BLACK;
-        } else {
-            return Color.WHITE;
-        }
+        return Color.hsb(color.getHue(), color.getSaturation()*0.8, 1-color.getBrightness());
     }
 
-    public Button(double x, double y, double width, double height, String text, Color color, Runnable runnable){
+    public Button(double x, double y, double width, double height, double outlineSize, double fontSize, String text, Color color, Runnable runnable){
         fillColor = color;
         outlineColor = toOutlineColor(color);
         textColor = toTextColor(color);
@@ -41,16 +42,30 @@ public class Button {
         this.y = y;
         this.width = width;
         this.height = height;
+        this.outlineSize = outlineSize;
+        this.fontSize = fontSize;
     }
 
 
 
     public void show(GraphicsContext gc){
+        shown = true;
 
+        gc.setFill(outlineColor);
+        gc.fillRect(x, y, width, height);
+
+        gc.setFill(fillColor);
+        gc.fillRect(x+outlineSize, y+outlineSize, width - (outlineSize * 2), height - (outlineSize*2));
+
+
+        gc.setFill(textColor);
+        gc.setTextAlign(TextAlignment.CENTER);
+        gc.setFont(Font.font("arial", FontWeight.BOLD, fontSize));
+        gc.fillText(text, x + (width/2), y + ((fontSize * 3/4) + height)/2);
     }
 
     public void hide(){
-
+        shown = false;
     }
 
     public boolean isClicled(){
--- src/Graphics/MainMenu.java
+++ src/Graphics/MainMenu.java
@@ -4,6 +4,9 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class MainMenu {
+    private Button test = new Button(100,100,200,100,10,40,"PLAY", Color.GREENYELLOW, null);
+
     public void doFrame(GraphicsContext gc){
+        test.show(gc);
     }
 }
